93e8b414b0b6f21dbc36105b5a64f20d8feab1bd
Alden Lu <business.aldenlu@gmail.com>
2025-05-23 15:43:35 +0000

    added breaking up text nodes via delimiter + tests

--- src/converter.py
+++ src/converter.py
@@ -1,4 +1,7 @@
+from typing import List
+from htmlnode import HTMLNode
 from textnode import TextNode, TextType
+import re
 # from htmlnode import
 from leafnode import LeafNode
 def text_node_to_html_node(text_node: TextNode):
@@ -16,3 +19,41 @@ def text_node_to_html_node(text_node: TextNode):
         return LeafNode("img",text_node.text,None,None)
     else:
         raise NotImplementedError(f"Did not implement for {text_node}")
+def split_nodes_delimiter(old_nodes: List[TextNode], delimiter, text_type)->List[TextNode]:
+    res = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            res.append(old_node)
+            continue
+        split_nodes = [] # stores the nodes we split
+        sections = old_node.text.split(delimiter)
+        for i in range(len(sections)):
+            value = sections[i] 
+            if value == "":
+                continue
+            if i % 2 == 0:
+                # create text
+                split_nodes.append(TextNode(value, TextType.TEXT))
+            else:  
+                split_nodes.append(TextNode(value, text_type))
+        res.extend(split_nodes)
+    return res
+def extract_markdown_images(text):
+    # text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    # print(extract_markdown_images(text))
+    # [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+    ""
+    regex_string = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(regex_string, text)
+    return matches
+    
+def extract_markdown_links(text):
+    regex_string = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(regex_string, text)
+    return matches
+
+def split_nodes_link(node:List[TextNode]):
+    pass
+    # extract_markdown_links()
+        
--- src/test_converter.py
+++ src/test_converter.py
@@ -1,6 +1,6 @@
 from unittest import TestCase, main
 
-from converter import text_node_to_html_node
+from converter import extract_markdown_images, split_nodes_delimiter, text_node_to_html_node
 from textnode import TextNode, TextType
 
 
@@ -55,7 +55,31 @@ class converterTest(TestCase):
             self.assertEqual(html_node.value, "This is a image node")
         else:
             raise TypeError("text_node_to_html_node fails to convert text node")
-        
+    
+    def test_split_node_delimiter_end_with_delimiter(self):
 
+        node = TextNode("`This is a code block`",TextType.TEXT)
+        split_array = split_nodes_delimiter([node],"`",TextType.CODE)
+        ans = "TextNode(This is a code block, code_text, None)"
+        self.assertEqual(repr(split_array[0]), ans )
+    def test_split_node_delimiter_between_text(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        # self.assertEqual(new_nodes[0],TextNode("This is text with a ", TextType.TEXT))
+        # self.assertEqual(new_nodes[1],TextNode("code block", TextType.CODE))
+        # self.assertEqual(new_nodes[2],TextNode(" word", TextType.TEXT))
+
+        ans = [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ]
+        self.assertEqual(ans,new_nodes)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 if __name__ == "__main__":
     main()
