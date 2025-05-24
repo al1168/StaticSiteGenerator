343bca18e6431a4c31828c6f5ad68d2ac4266010
Alden Lu <business.aldenlu@gmail.com>
2025-05-24 14:20:21 +0000

    added link and image extraction. added text_to_textnodes

--- src/converter.py
+++ src/converter.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 from htmlnode import HTMLNode
 from textnode import TextNode, TextType
 import re
@@ -40,10 +40,6 @@ def split_nodes_delimiter(old_nodes: List[TextNode], delimiter, text_type)->List
         res.extend(split_nodes)
     return res
 def extract_markdown_images(text):
-    # text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-    # print(extract_markdown_images(text))
-    # [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
-    ""
     regex_string = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(regex_string, text)
     return matches
@@ -53,7 +49,114 @@ def extract_markdown_links(text):
     matches = re.findall(regex_string, text)
     return matches
 
-def split_nodes_link(node:List[TextNode]):
-    pass
-    # extract_markdown_links()
+def seperate_text_and_link(text) -> tuple[str,str]:
+    text_link = r"\[(.*?)\]"
+    link_text = r"\((.*?)\)"
+    text_value = re.findall(text_link, text)[0]
+    link_value = re.findall(link_text, text)[0]
+    return (text_value, link_value)
+def printlist(thelist):
+    for textNode in thelist:
+        print(textNode)
+        print("")
+    
+def split_nodes_link(old_nodes:List[TextNode]):
+    regex_string = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    res = []
+
+    for node in old_nodes:
+        text = node.text
+        # text_links_tuple = extract_markdown_links(text)
+        matches = re.finditer(regex_string, text)
+
+
+        spans = []
+        for match in matches:
+            start, end = match.span()
+            spans.append((start, end))
         
+            # need to extract out the text of the link and the value
+            # text_value, link_value = seperate_text_and_link(text[start:end])
+        if spans == []:
+            res.append(node)
+            continue
+        starting = 0
+        
+        for span_start, span_end in spans:
+            # check if starting matches
+            if span_start != starting and 0 <= starting < len(text):
+                normal_text_section = text[starting: span_start]
+                res.append(TextNode(normal_text_section ,TextType.TEXT, None))
+                # part of node _link
+            linked_string = text[span_start:span_end]
+            text_value, link_value = seperate_text_and_link(linked_string)
+            link_text_node = TextNode(text_value, TextType.LINK,link_value)
+            res.append(link_text_node)
+            starting = span_end
+        if starting != len(text):
+            normal_text_section = text[starting: starting: len(text)]
+            res.append(TextNode(normal_text_section ,TextType.TEXT, None))
+    # print(f"res: {res}")
+    # print("start link")
+    # printlist(res) 
+    # print("end link")
+    return res 
+
+
+            
+        
+def split_nodes_image(old_nodes:List[TextNode]):
+    regex_string = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    res = []
+    print("start image")
+    printlist(old_nodes) 
+    print("end image")
+    for node in old_nodes:
+        text = node.text
+        # text_links_tuple = extract_markdown_links(text)
+        matches = re.finditer(regex_string, text)
+        # print(f"matches: {list(matches)}")
+        spans = []
+        for match in matches:
+            start, end = match.span()
+            spans.append((start, end))
+            # print(f"Here: {text[start:end]}")
+            # need to extract out the text of the link and the value
+            # text_value, link_value = seperate_text_and_link(text[start:end])
+        
+        if spans == []:
+            res.append(node)
+            continue
+        starting = 0
+        for span_start, span_end in spans:
+            # check if starting matches
+            if span_start != starting and 0 <= starting < len(text):
+                normal_text_section = text[starting: span_start]
+                res.append(TextNode(normal_text_section ,TextType.TEXT, None))
+                # part of node _link
+            linked_string = text[span_start:span_end]
+            text_value, link_value = seperate_text_and_link(linked_string)
+            link_text_node = TextNode(text_value, TextType.IMAGE, link_value)
+            res.append(link_text_node)
+            starting = span_end
+        if starting != len(text):
+            normal_text_section = text[starting: len(text)]
+            res.append(TextNode(normal_text_section ,TextType.TEXT, None))
+
+    return res
+
+def text_to_textnodes(text):
+    # old nodes delmited , TEXT TYPE
+    delmiter_to_type = {
+        "`" : TextType.CODE,
+        "**": TextType.BOLD,
+        "_" : TextType.ITALIC,
+    }
+    curr_nodes = [TextNode(text,TextType.TEXT)]
+    for key, value in delmiter_to_type.items():
+        curr_nodes = split_nodes_delimiter(curr_nodes,key,value)
+        # print(f"text_type: {value}, curr_nodes: {curr_nodes}")
+    curr_nodes = split_nodes_image(curr_nodes)
+    # printlist(curr_nodes)
+    curr_nodes = split_nodes_link(curr_nodes)
+    return curr_nodes
--- src/test_converter.py
+++ src/test_converter.py
@@ -1,85 +1,132 @@
 from unittest import TestCase, main
 
-from converter import extract_markdown_images, split_nodes_delimiter, text_node_to_html_node
+from converter import extract_markdown_images, split_nodes_delimiter, split_nodes_image, split_nodes_link, text_node_to_html_node, text_to_textnodes
 from textnode import TextNode, TextType
 
 
 
 class converterTest(TestCase):
 
-    def test_convert_text(self):
-        node = TextNode("This is a text node", TextType.TEXT)
-        html_node = text_node_to_html_node(node)
-        if html_node: 
-            self.assertEqual(html_node.value, "This is a text node")
-        else:
-            raise TypeError("text_node_to_html_node fails to convert text node")
-    def test_convert_bold(self):
-        node = TextNode("This is a bold node", TextType.BOLD)
-        html_node = text_node_to_html_node(node)
-        if html_node: 
-            self.assertEqual(html_node.tag, "b")
-            self.assertEqual(html_node.value, "This is a bold node")
-        else:
-            raise TypeError("text_node_to_html_node fails to convert text node")
-    def test_convert_italic(self):
-        node = TextNode("This is a italic node", TextType.ITALIC)
-        html_node = text_node_to_html_node(node)
-        if html_node: 
-            self.assertEqual(html_node.tag, "i")
-            self.assertEqual(html_node.value, "This is a italic node")
-        else:
-            raise TypeError("text_node_to_html_node fails to convert text node")
-    def test_convert_code(self):
-        node = TextNode("This is a code node", TextType.CODE)
-        html_node = text_node_to_html_node(node)
-        if html_node: 
-            self.assertEqual(html_node.tag, "code")
-            self.assertEqual(html_node.value, "This is a code node")
-        else:
-            raise TypeError("text_node_to_html_node fails to convert text node")
-    def test_convert_link(self):
-        node = TextNode("This is a link node", TextType.LINK)
-        html_node = text_node_to_html_node(node)
-        if html_node: 
-            self.assertEqual(html_node.tag, "a")
-            self.assertEqual(html_node.value, "This is a link node")
-        else:
-            raise TypeError("text_node_to_html_node fails to convert text node")
+    # def test_convert_text(self):
+    #     node = TextNode("This is a text node", TextType.TEXT)
+    #     html_node = text_node_to_html_node(node)
+    #     if html_node: 
+    #         self.assertEqual(html_node.value, "This is a text node")
+    #     else:
+    #         raise TypeError("text_node_to_html_node fails to convert text node")
+    # def test_convert_bold(self):
+    #     node = TextNode("This is a bold node", TextType.BOLD)
+    #     html_node = text_node_to_html_node(node)
+    #     if html_node: 
+    #         self.assertEqual(html_node.tag, "b")
+    #         self.assertEqual(html_node.value, "This is a bold node")
+    #     else:
+    #         raise TypeError("text_node_to_html_node fails to convert text node")
+    # def test_convert_italic(self):
+    #     node = TextNode("This is a italic node", TextType.ITALIC)
+    #     html_node = text_node_to_html_node(node)
+    #     if html_node: 
+    #         self.assertEqual(html_node.tag, "i")
+    #         self.assertEqual(html_node.value, "This is a italic node")
+    #     else:
+    #         raise TypeError("text_node_to_html_node fails to convert text node")
+    # def test_convert_code(self):
+    #     node = TextNode("This is a code node", TextType.CODE)
+    #     html_node = text_node_to_html_node(node)
+    #     if html_node: 
+    #         self.assertEqual(html_node.tag, "code")
+    #         self.assertEqual(html_node.value, "This is a code node")
+    #     else:
+    #         raise TypeError("text_node_to_html_node fails to convert text node")
+    # def test_convert_link(self):
+    #     node = TextNode("This is a link node", TextType.LINK)
+    #     html_node = text_node_to_html_node(node)
+    #     if html_node: 
+    #         self.assertEqual(html_node.tag, "a")
+    #         self.assertEqual(html_node.value, "This is a link node")
+    #     else:
+    #         raise TypeError("text_node_to_html_node fails to convert text node")
         
-    def test_convert_image(self):
-        node = TextNode("This is a image node", TextType.IMAGE)
-        html_node = text_node_to_html_node(node)
-        if html_node: 
-            self.assertEqual(html_node.tag, "img")
-            self.assertEqual(html_node.value, "This is a image node")
-        else:
-            raise TypeError("text_node_to_html_node fails to convert text node")
+    # def test_convert_image(self):
+    #     node = TextNode("This is a image node", TextType.IMAGE)
+    #     html_node = text_node_to_html_node(node)
+    #     if html_node: 
+    #         self.assertEqual(html_node.tag, "img")
+    #         self.assertEqual(html_node.value, "This is a image node")
+    #     else:
+    #         raise TypeError("text_node_to_html_node fails to convert text node")
     
-    def test_split_node_delimiter_end_with_delimiter(self):
+    # def test_split_node_delimiter_end_with_delimiter(self):
 
-        node = TextNode("`This is a code block`",TextType.TEXT)
-        split_array = split_nodes_delimiter([node],"`",TextType.CODE)
-        ans = "TextNode(This is a code block, code_text, None)"
-        self.assertEqual(repr(split_array[0]), ans )
-    def test_split_node_delimiter_between_text(self):
-        node = TextNode("This is text with a `code block` word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        # self.assertEqual(new_nodes[0],TextNode("This is text with a ", TextType.TEXT))
-        # self.assertEqual(new_nodes[1],TextNode("code block", TextType.CODE))
-        # self.assertEqual(new_nodes[2],TextNode(" word", TextType.TEXT))
+    #     node = TextNode("`This is a code block`",TextType.TEXT)
+    #     split_array = split_nodes_delimiter([node],"`",TextType.CODE)
+    #     ans = "TextNode(This is a code block, code_text, None)"
+    #     self.assertEqual(repr(split_array[0]), ans )
+    # def test_split_node_delimiter_between_text(self):
+    #     node = TextNode("This is text with a `code block` word", TextType.TEXT)
+    #     new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
 
+    #     ans = [
+    #             TextNode("This is text with a ", TextType.TEXT),
+    #             TextNode("code block", TextType.CODE),
+    #             TextNode(" word", TextType.TEXT),
+    #         ]
+    #     self.assertEqual(ans,new_nodes)
+
+    # def test_extract_markdown_images(self):
+    #     matches = extract_markdown_images(
+    #         "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+    #     )
+    #     self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+
+    # def test(self):
+    #     node = TextNode(
+    #     "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+    #     TextType.TEXT,
+    #     )
+    #     new_nodes = split_nodes_link([node])
+    #     ans = [
+    #         TextNode("This is text with a link ", TextType.TEXT),
+    #         TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+    #         TextNode(" and ", TextType.TEXT),
+    #         TextNode(
+    #             "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+    #         ),
+    #     ]
+    #     self.assertEqual(new_nodes, ans) 
+    # def test_image(self):
+    #     node = TextNode(
+    #     "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+    #     TextType.TEXT,
+    # )
+    #     new_nodes = split_nodes_image([node])
+    #     self.assertListEqual(
+    #     [
+    #         TextNode("This is text with an ", TextType.TEXT),
+    #         TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+    #         TextNode(" and another ", TextType.TEXT),
+    #         TextNode(
+    #             "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+    #         ),
+    #     ],
+    #     new_nodes,
+    # )
+    def test_full(self):
+        s = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        myans = text_to_textnodes(s)
         ans = [
-                TextNode("This is text with a ", TextType.TEXT),
-                TextNode("code block", TextType.CODE),
-                TextNode(" word", TextType.TEXT),
-            ]
-        self.assertEqual(ans,new_nodes)
-
-    def test_extract_markdown_images(self):
-        matches = extract_markdown_images(
-            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
-        )
-        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+        TextNode("This is ", TextType.TEXT),
+        TextNode("text", TextType.BOLD),
+        TextNode(" with an ", TextType.TEXT),
+        TextNode("italic", TextType.ITALIC),
+        TextNode(" word and a ", TextType.TEXT),
+        TextNode("code block", TextType.CODE),
+        TextNode(" and an ", TextType.TEXT),
+        TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+        TextNode(" and a ", TextType.TEXT),
+        TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(ans, myans)
 if __name__ == "__main__":
     main()
